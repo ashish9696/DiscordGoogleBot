9d5ab348e6f4d99a7324a15a6ef166d5733a404b
ashish9696 <am67663@gmail.com>
2020-09-06 15:30:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,16 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100
 # mobile user-agent
 MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 
-DISCORD_TOKEN = "NzUxNzA0NzQ4MzEwMjY1OTE2.X1M9mQ.6QJpjvY5HmfcShDa9Ce3UCenXMM"#os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = "ADD_YOUR_TOKEN"#os.getenv('DISCORD_TOKEN')
 
-USER='EPUgis3KQU'
+USER='DB_USERNAME'
 
-DBNAME='EPUgis3KQU'
+DBNAME='DB_NAME'
 
-PASSWORD='jrNpNAE8Gc'
+PASSWORD='DB_PASSWORD'
 
-SERVER='remotemysql.com'
-PORT='3306'
+SERVER='DB_HOST'
+PORT='DB_PORT'
 
-DBTABLE = 'usersearch'
-
-DBNAME='EPUgis3KQU'
+DBTABLE = 'DB_TABLE'
 
